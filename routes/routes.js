00b08d1c04f6ef2f1a0214ab342f7a53b5d281e1
DHEERAJ KUMAR PAMPARI <44135491+dheeraj234@users.noreply.github.com>
2022-08-23 23:08:43 +0530

    Add files via upload

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const router= express.Router()
+const signUpTemplateCopy= require('../models/signUpModels')
+
+router.post('/signup',(request,response)=>{
+    const signedUpUser= new signUpTemplateCopy({
+        fullName:request.body.fullName,
+        username:request.body.username,
+        email:request.body.email,
+        password:request.body.password
+    })
+    signedUpUser.save()
+    .then(data=>{response.json(data)})
+    .catch(err=>{response.json(err)})
+})
+
+module.exports= router
---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+const app= express();
+const mongoose = require('mongoose');
+const dotenv= require('dotenv');
+const routeurls= require('./routes/routes')
+const cors = require('cors')
+dotenv.config()
+
+mongoose.connect(process.env.DATABASE_ACCESS,()=>{console.log("database connected");})
+app.use(express.json());
+app.use(cors())
+app.use('/app',routeurls)
+app.listen(4000,()=>{
+    "port no 4000"
+});

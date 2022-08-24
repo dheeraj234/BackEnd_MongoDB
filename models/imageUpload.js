fd488dbfffbb5b744129f412a7b05f23c39d78a4
DHEERAJ KUMAR PAMPARI <44135491+dheeraj234@users.noreply.github.com>
2022-08-24 15:26:10 +0530

    Add files via upload

---
+++ models/imageUpload.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const imgSchema= new mongoose.Schema({
+    name:String,
+    img:{
+        data:Buffer,
+        contentType:String
+    }
+})
+module.exports= ImageModel= mongoose.model('Image',imgSchema)
